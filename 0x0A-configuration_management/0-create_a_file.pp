11fa680c55405d96bb21e85c37e929be41a9af5f
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-11-24 17:42:39 -0800

    created first puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file
+# create a file in /tmp
+
 file { '/tmp/school':
-    ensure  => 'file',
-    path    => '/tmp/school',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
