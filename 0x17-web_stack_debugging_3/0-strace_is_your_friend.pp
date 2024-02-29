b8ff5047c6627de351ea97c6fe5dec60b52014a7
Emmyglobal <nwaforugochukwu21@gmail.com>
2024-02-29 02:55:29 -0800

    fourth webstack fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# The fourth web-stack debug script in puppet
+# Fourth web-stack debug script in puppet
 exec { 'wsd3':
-	command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-	path => ['/bin', '/usr/bin']
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    =>  ['/bin', '/usr/bin']
 }
